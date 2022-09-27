76d077fc8aa5f77ee472fd7d25bfcdad0aa8655d
MSFPT <MsfNemati1386@gmail.com>
2022-09-27 05:40:10 +0330

    v1.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# file-to-base64-url
+
+![demo](demo.png)
+
+<br>
+
+## Clone from GitHub
+```bash
+git clone https://github.com/msfpt/file-to-base64-url
+```
+<br>
+
+## Run on localhost
+
+```bash
+php -S localhost:6613 -t "file-to-base64-url"
+```
+
+then can view it on [http://localhost:6613](http://localhost:6613)
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+    /* Coded By https://github.com/msfpt */
+
+    function template_render(String $template_file, Array $data, int $status = 200){
+        
+        $template_page = file_get_contents($template_file);
+    
+        foreach (array_keys($data) as $key => $search) {
+
+            $replace = $data[$search];
+
+            $template_page = str_replace($search, $replace, $template_page);
+
+        }
+
+        http_response_code($status);
+
+        echo $template_page;
+
+    }
+
+    if(!empty($_FILES['file'])){
+
+        $file_name = $_FILES['file']['name'];
+        $file_type = $_FILES['file']['type'];
+
+        template_render('template.html', array(
+            '{{ is_show }}' => '',
+            '{{ file_name }}' => $file_name,
+            '{{ base64_url }}' => "data:$file_type;base64,".base64_encode(file_get_contents($_FILES['file']['tmp_name']))
+        ), 200);
+
+    } else {
+
+        template_render('template.html', array('{{ is_show }}' => 'hide'), 200);
+
+    }
+
+?>
